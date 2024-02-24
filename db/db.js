be285802e3358b6c1be84be7186119313d06a1ae
Ramiro Padró <ramipadro93@gmail.com>
2024-02-24 15:08:48 -0300

    prueba

--- db/db.js
+++ db/db.js
@@ -3,12 +3,20 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL, // Tu cadena de conexión
+  ssl: {
+    rejectUnauthorized: true, // Esto verifica que el certificado SSL del servidor sea válido
+    sslmode: 'require' // Asegúrate de que la conexión use SSL
+  }
 });
 
 pool.connect((err, client, release) => {
